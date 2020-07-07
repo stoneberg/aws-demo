e49bf9c7abdb017c2f762fdebdde87ec5f8dcf7d
Lee YuPyeong <stoneberg73@gmail.com>
2020-07-07 13:07:01 +0900

    메인 화면 및 테스트 추가

---
+++ src/test/java/me/stone/aws/play/web/controller/IndexControllerTest.java
@@ -0,0 +1,28 @@
+package me.stone.aws.play.web.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    @DisplayName("메인페이지 로딩 테스트")
+    void loadingPageTest() {
+        // when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        // then
+        assertThat(body).contains("스프링부트로 시작하는 웹서비스");
+    }
+
+}
