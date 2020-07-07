ad7910fd6dac056fb28b0fd7fcf8dda4d4445133
Lee YuPyeong <stoneberg73@gmail.com>
2020-07-07 14:24:18 +0900

    글 등록 추가

--- src/main/java/me/stone/aws/play/AwsDemoApplication.java
+++ src/main/java/me/stone/aws/play/AwsDemoApplication.java
@@ -2,7 +2,9 @@ package me.stone.aws.play;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AwsDemoApplication {
 
