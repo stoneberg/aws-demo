568419313b275245e2e99e39aeced27c3dc6014a
Lee YuPyeong <stoneberg73@gmail.com>
2020-07-13 02:27:07 +0900

    google oauth2 추가

---
+++ src/main/java/me/stone/aws/play/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package me.stone.aws.play.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import me.stone.aws.play.common.enums.Role;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                //.headers().frameOptions().disable().and() // h2-console enabled
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**")
+                .permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+
+    }
+}
---
+++ src/main/java/me/stone/aws/play/user/domain/User.java
@@ -0,0 +1,51 @@
+package me.stone.aws.play.user.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.stone.aws.play.common.entity.BaseTimeEntity;
+import me.stone.aws.play.common.enums.Role;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
